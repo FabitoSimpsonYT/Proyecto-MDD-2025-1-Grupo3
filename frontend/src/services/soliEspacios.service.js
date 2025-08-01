bc99a1892290029bb2c90ccf7bc4345942c6ea77
Fabián Mora Carrasco <fabian.mora2301@alumnos.ubiobio.cl>
2025-08-01 00:20:56 -0400

    Update soliEspacios.service.js

--- frontend/src/services/soliEspacios.service.js
+++ frontend/src/services/soliEspacios.service.js
@@ -2,9 +2,7 @@ import axios from '@services/root.service.js'
 
 export async function getSoliResidente(){
     try {
-        console.log('Consultando URL:', `/soliEspacios/residente`);
         const response = await axios.get(`/soliEspacios/residente`);
-        console.log('Respuesta del backend:', response.data);
         return response.data.data;
     } catch (error) {
         console.error("Error al obtener solicitudes:", error);
@@ -13,9 +11,7 @@ export async function getSoliResidente(){
 
 export async function deleteSoli(idSolicitud) {
     try {
-        console.log('Consultando URL:', `/soliEspacios/${idSolicitud}`);
         const response = await axios.delete(`/soliEspacios/${idSolicitud}`);
-        console.log('Respuesta del backend:', response.data);
         return response.data;
     } catch (error) {
         console.error("Error al eliminar solicitud:", error);
@@ -24,9 +20,7 @@ export async function deleteSoli(idSolicitud) {
 
 export async function updateSoli(idSolicitud, updatedValues) {
     try {
-        console.log('Consultando URL:', `/soliEspacios/${idSolicitud}`);
         const response = await axios.put(`/soliEspacios/${idSolicitud}`, updatedValues);
-        console.log('Respuesta del backend:', response.data);
         return response.data; 
     } catch (error) {
         console.error("Error al actualizar solicitud:", error);
@@ -36,9 +30,7 @@ export async function updateSoli(idSolicitud, updatedValues) {
 
 export async function createSoli(solicitud) {
     try {
-        console.log('Consultando URL:', `/soliEspacios`);
         const response = await axios.post(`/soliEspacios`, solicitud);
-        console.log('Respuesta del backend:', response.data);
         return response.data;
     } catch (error) {
         console.error("Error al crear solicitud:", error);
